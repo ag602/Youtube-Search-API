b8bd1e0b794c640a82d194bb481163350d3f30af
akul <akul.gupta.phe17@itbhu.ac.in>
2020-10-03 01:41:54 +0530

    Removed arrow import from views

--- youtubeapi/views.py
+++ youtubeapi/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import *
 from django.core.paginator import Paginator
 # Create your views here.
-import arrow
+# import arrow
 def home(request):
     c = Vidata.objects.order_by('-publishedAt').all()
     paginator = Paginator(c, 10) # Show 25 contacts per page.
